8a6a5bfa09067cf09641938da6429099748e77a9
MAzisSaputra <muhammadazis41103@gmail.com>
2021-01-05 01:32:06 +0700

    Membuat komponen sederhana

--- App.js
+++ App.js
@@ -1,89 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {Text, View, Image, TextInput} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <Text>hello world...</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}} />
+      <Text>Akmal</Text>
+      <Text>Rino</Text>
+      <Azis />
+      <Text>Iyaw</Text>
+      <Text>Rohan</Text>
+      <Text>Zidan</Text>
+      <Photo />
+      <TextInput style={{borderWidth: 1}}/>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const Azis = () => {
+  return <Text>Azis</Text>;
+};
 
+const Photo = () => {
+  return (
+    <Image 
+    source={{uri: 'https://placeimg.com/100/100/tech'}} 
+    style={{width: 100, height: 100}}/
+    >
+  );
+};
 export default App;
